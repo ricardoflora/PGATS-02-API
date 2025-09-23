5fa7d3a496cd9e9de47b6544d074316f9c3c94aa
Ricardo <ricardoflora@gmail.com>
2025-09-23 16:53:07 -0300

    Commit Aula

---
+++ test/controller/transferController.test.js
@@ -0,0 +1,20 @@
+const request  = require('supertest');
+const sinon    = require('sinon');
+const chai     = require('chai');
+const expect   = chai.expect;
+const app               = require('../../app');
+
+describe('Transfer Controller', () => {
+    describe(' POST /transfer', () => {
+it('Quando informo ', async () => {
+    const resposta = await request(app)
+        .post('/transfer')
+        .send({
+            from: "Ricardo ",
+            to: "Pedro",
+            amount: 100
+        });
+    expect(resposta.status).to.equal(201);
+});
+
+});
