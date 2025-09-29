a0c58f8a48bbeffadf619c2a5bf3f42962e4326c
Ricardo Flora <ricardoflora@gmail.com>
2025-09-29 20:38:28 -0300

    teste

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -2,19 +2,19 @@ const request  = require('supertest');
 const sinon    = require('sinon');
 const chai     = require('chai');
 const expect   = chai.expect;
-const app               = require('../../app');
+const app      = require('../../app');
 
 describe('Transfer Controller', () => {
-    describe(' POST /transfer', () => {
-it('Quando informo ', async () => {
-    const resposta = await request(app)
-        .post('/transfer')
-        .send({
-            from: "Ricardo ",
-            to: "Pedro",
-            amount: 100
+    describe('POST /transfer', () => {
+        it('Quando informo ', async () => {
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                 from: "Ricardo ",
+                 to: "Pedro",
+                 amount: 100
+                });
+            expect(resposta.status).to.equal(400);
         });
-    expect(resposta.status).to.equal(201);
-});
-
+    });
 });
